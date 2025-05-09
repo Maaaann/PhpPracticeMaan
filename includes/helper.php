189d93a7ba43afb513b46279e4a2f7b499505b22
Abdelrahman Altayeb <38187446+Maaaann@users.noreply.github.com>
2025-05-10 01:34:22 +0300

    Initial commit

---
+++ includes/helper.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!function_exists('db_create')){
+    function db_create($table,array $data){
+        $sql = "";
+
+        mysqli_query($GLOBALS['connect'],$sql);
+        
+
+    }   
+}
+
+db_create([]); 
