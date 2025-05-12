4ae8edb0b3cd5f3044bbaf99f21f8dd1c4e9d1d2
Abdelrahman Altayeb <38187446+Maaaann@users.noreply.github.com>
2025-05-13 01:21:27 +0300

    fetching the data

--- includes/helper.php
+++ includes/helper.php
@@ -21,8 +21,6 @@ if(!function_exists('db_create')){
         mysqli_query($GLOBALS['connect'],$sql);
         $id =  mysqli_insert_id($GLOBALS['connect']);
         $first = mysqli_query($GLOBALS['connect'],"select * from ".$table." where id=".$id);
-        
-        // data is converted into associative array by fetching it with this funciton
         $data= mysqli_fetch_assoc($first);
         mysqli_close($GLOBALS['connect']);
         return $data;
@@ -34,20 +32,20 @@ if(!function_exists('db_create')){
 if (! function_exists('db_update')){
     function db_update(string $table , array $data , int $id){
         $sql = "update ".$table." set ";
-        $columns = '';
-        //$values = '';
+        $column_value = '';
 
         foreach($data as $key=>$value){
-            $columns .= $key."'='".$value."' and '"; 
-            //$values .=" '".$value."',"; 
+            $column_value .= $key."'='".$value."' , '"; 
         }
     
-        $columns = rtrim($columns,'and ');
-        //$values = rtrim($values,',');
-        $sql .= $columns." where id=".$id;
+        $column_value = rtrim($column_value,',');
+        $sql .= $column_value." where id=".$id.";";
         echo $sql;
-        //mysqli_query($GLOBALS['connect'],$sql);
-
+        mysqli_query($GLOBALS['connect'],$sql);
+        $first = mysqli_query($GLOBALS['connect'],"select * from ".$table." where id=".$id);
+        $data= mysqli_fetch_assoc($first);
+        mysqli_close($GLOBALS['connect']);
+        return $data;
 
 
     }
