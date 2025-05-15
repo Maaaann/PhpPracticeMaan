7aa2d32747b86f251350a2ca1b9dfa2a8893333d
Abdelrahman Altayeb <38187446+Maaaann@users.noreply.github.com>
2025-05-16 00:17:42 +0300

    Update helper.php

--- includes/helper.php
+++ includes/helper.php
@@ -53,7 +53,7 @@ if (! function_exists('db_update')){
 
 
 //---------------Deleting Data Query-------------//
-//DELETE FROM table_name WHERE id=
+//DELETE FROM table_name WHERE id= $id
 
 if(!function_exists('db_delete')){
     function db_delete(string $table, int $id){
